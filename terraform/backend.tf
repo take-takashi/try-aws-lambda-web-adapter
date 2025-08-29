604d121db9b0ff148007e5cfe0a932fe4e8fb6dd
TAKE Takashi <puller.swipes1h@icloud.com>
2025-08-29 20:44:23 +0900

    重複したプロバイダ設定を削除

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
     # Configuration for bucket, region, and dynamodb_table
     # will be provided via the 'mise run tf-init' task.
